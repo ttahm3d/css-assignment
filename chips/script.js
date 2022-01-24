537a92b8f6869bc23beb0f05ed05373a884f3428
ttahm3d <ttahm3d@gmail.com>
2022-01-24 21:16:56 +0530

    feat: created chips

---
+++ chips/script.js
@@ -0,0 +1,29 @@
+const chipsContainer = document.querySelector(".chips-container");
+const inputBox = document.querySelector(".chip-input-box");
+const div = document.createElement("div");
+const form = document.querySelector("#form");
+
+form.addEventListener("submit", (e) => {
+	const chipsList = [];
+	e.preventDefault();
+	chipsList.push(inputBox.value);
+	inputBox.value = "";
+
+	chipsList.map(
+		(chip) =>
+			(chipsContainer.innerHTML += `
+      <div class="chip">
+        <p>${chip}</p>
+				<i class="fas fa-times-circle close-btn" aria-hidden="true"></i>
+      </div>
+    `)
+	);
+
+	const removeChip = document.querySelectorAll(".close-btn");
+
+	for (i = 0; i < removeChip.length; i++) {
+		removeChip[i].addEventListener("click", (e) => {
+			chipsContainer.removeChild(e.target.parentNode);
+		});
+	}
+});
